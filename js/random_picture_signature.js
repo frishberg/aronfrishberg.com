6f5dfe9eab142606f3366feefff42fd1beba488e
A-ron <aronf32@gmail.com>
2022-02-10 16:39:07 -0500

    fixing strange images bug

--- js/random_picture_signature.js
+++ js/random_picture_signature.js
@@ -5,18 +5,18 @@ var random2 = Math.floor(Math.random() * (num_of_signatures));
 
 window.onload = function() {
     if (sessionStorage.getItem('picture')==null) {
-        document.getElementById("thejewhimself").src="../img/aron/"+random1+".jpg";
+        document.getElementById("thejewhimself").src="img/aron/"+random1+".jpg";
         sessionStorage.setItem('picture', random1);
     }
     else {
-        document.getElementById("thejewhimself").src="../img/aron/"+sessionStorage.getItem('picture')+".jpg";
+        document.getElementById("thejewhimself").src="img/aron/"+sessionStorage.getItem('picture')+".jpg";
     }
     if (sessionStorage.getItem('signature')==null) {
-        document.getElementById("signature").src="../img/signatures/"+random2+".png";
+        document.getElementById("signature").src="img/signatures/"+random2+".png";
         sessionStorage.setItem('signature', random2);
     }
     else {
-        document.getElementById("signature").src="../img/signatures/"+sessionStorage.getItem('signature')+".png";
+        document.getElementById("signature").src="img/signatures/"+sessionStorage.getItem('signature')+".png";
     }
 
     //admin mode
