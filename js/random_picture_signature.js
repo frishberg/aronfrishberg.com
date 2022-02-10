78a594048ac85506cc88e5a6c760b88292be6e5d
A-ron <aronf32@gmail.com>
2022-02-10 14:28:53 -0500

    fixing picture script

--- js/random_picture_signature.js
+++ js/random_picture_signature.js
@@ -1,4 +1,4 @@
-var num_of_pics = 7
+var num_of_pics = 3
 var random1 = Math.floor(Math.random() * (num_of_pics));
 var num_of_signatures = 9
 var random2 = Math.floor(Math.random() * (num_of_signatures));
